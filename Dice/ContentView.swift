d4b6aa918c3700055d5c619767e199a263319958
baskoro aji susetyo <baskoroajisusetyo@gmail.com>
2020-02-06 16:51:05 +0700

    add code

---
+++ Dice/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  Dice
+//
+//  Created by Baskoro on 06/02/20.
+//  Copyright © 2020 Baskoro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDice:Int = 1
+    @State var rightDice:Int = 1
+    
+    var body: some View {
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+           
+            VStack{
+                Image("diceeLogo")
+                    .aspectRatio(1,contentMode: .fit)
+                Spacer()
+                HStack{
+                    DiceView(n:self.leftDice)
+                    DiceView(n:self.rightDice)
+                }
+                Spacer()
+                Button(action: {
+                    self.leftDice = Int.random(in:1...6)
+                    self.rightDice = Int.random(in:1...6)
+                }){
+                    Text("Roll")
+                        .font(.system(size:50))
+                        .foregroundColor(Color.white)
+                        .fontWeight(.heavy)
+                        .padding(.horizontal)
+                }
+                    .background(Color.red)
+                    
+            }
+          
+        }
+    }
+}
+
+struct DiceView:View{
+    let n:Int
+    
+    var body: some View{
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1,contentMode: .fit)
+            .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
